c1f4182eec0d6ec93d9056dbcb9c38970ade6aec
Daniel Augusto <dan.engsoftware@proton.me>
2024-10-19 21:08:28 -0300

    Corrigindo o reconhecimento de áudio

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import speech_recognition as sr
 import pyttsx3
+import time
 
 
 # Fala
@@ -22,19 +23,34 @@ def voice(message, rate=150, volume=1.0, voice_id=1):
 
 # Escuta
 def speech_recognition():
+    print("Iniciando o recognizer")
     recognizer = sr.Recognizer()
+    print("Recognizer funcionando")
 
     with sr.Microphone() as source:
+        print("Ajustando para o ruído de ambiente...")
         recognizer.adjust_for_ambient_noise(source, duration=1)
-        voice('Estou ouvindo, você precisa de algo?')
-        audio = recognizer.listen(source, timeout=10, phrase_time_limit=10)
+        voice('Estou ouvindo')
 
-    try:
-        message = recognizer.recognize_sphinx(audio)
-        print(f'Você disse: {message}')
-    except sr.UnknownValueError:
-        voice('Desculpe, não consegui entender o que você disse, pode tentar de novo?')  
-    except sr.RequestError as e:
-        voice(f'Parece que houve um problema com o serviço. Tente novamente mais tarde. {e}')
+        while True:
+            print("Ouvindo...")
+            try:
+                audio = recognizer.listen(source, timeout=10)
+                print("Áudio captado")
+
+                message = recognizer.recognize_sphinx(audio)
+                print(f'Você disse: {message}')
+
+                if "parar" in message.lower():
+                    voice("Encerrando a assistente")
+                    break
+
+            except sr.WaitTimeoutError:
+                voice("Tempo de espera esgotado")
+            except sr.UnknownValueError:
+                voice('Desculpe, não consegui entender o que você disse, pode tentar de novo?')  
+            except sr.RequestError as e:
+                print(f'Parece que houve um problema com o serviço. Tente novamente mais tarde. {e}')
+            time.sleep(1)
 
 speech_recognition()
